be2f6eff0a74e2325d63f996a68fcc1ebd881192
Ishan Kamat <ishankamat123@gmail.com>
2018-02-28 20:07:41 -0600

    changed tab titles
    theyre actually accurate now (pulled the images off the apple dev info page)

--- MainViewController.swift
+++ MainViewController.swift
@@ -13,8 +13,7 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+    
 
         // Do any additional setup after loading the view.
     }
